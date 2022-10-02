67866f0f5b8e3c7e1b34b4b0d0bb013587cdb762
Your Name <you@example.com>
2022-10-03 01:58:19 +0530

    sc

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const textAreaEle = document.querySelector(".main-container textarea");
 const btnEle = document.querySelector(".main-container div button");
 const resultEle = document.querySelector(".main-container div #result");
 btnEle.addEventListener("click", function () {
-  const inputString = textAreaEle.value;
+  const inputString = textAreaEle.value.toLowerCase();
   const splittedString = inputString.split("");
   const reversedString = splittedString.reverse();
   const reverseJoinedString = reversedString.join("");
